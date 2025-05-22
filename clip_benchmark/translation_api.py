e090dcd6b2225f2791ff53b4c6bdd917049500d8
YasineDeghaies <yasine.deghaies@gmail.com>
2025-05-22 06:43:41 +0200

    Final changes

--- clip_benchmark/translation_api.py
+++ clip_benchmark/translation_api.py
@@ -3,13 +3,11 @@ import deepl
 from pathlib import Path
 
 auth = os.getenv("DEEPL_AUTH_KEY")
-if not auth:
-    raise RuntimeError("Set the DEEPL_AUTH_KEY environment variable")
 
 translator = deepl.Translator(auth)
 SOURCE_LANG, TARGET_LANG = "DE", "EN-US"
 
-batch_size = 50                          # DeepL limit: ≤50 texts/request
+batch_size = 50                          
 translated = []
 
 captions_file_path = "/vol/fob-vol3/mi20/deghaisa/code/CLIP_benchmark/clip_benchmark/root/crossmodal3600_captions-de.json"
@@ -21,7 +19,7 @@ for i in range(0, len(captions), batch_size):
     batch = captions[i : i + batch_size]
     results = translator.translate_text(
         batch,
-        source_lang=SOURCE_LANG,
+        # source_lang=None, 
         target_lang=TARGET_LANG,
     )
     translated.extend(r.text for r in results)
